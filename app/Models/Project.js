5ae4814826535052e98e9cd631e2eb6bb3aac353
denisprado <denisforigo@gmail.com>
2019-11-12 16:11:08 -0300

    Categories added

--- app/Models/Project.js
+++ app/Models/Project.js
@@ -7,6 +7,12 @@ class Project extends Model {
   files () {
     return this.hasMany('App/Models/File')
   }
+
+  categories () {
+    return this.belongsToMany('App/Models/Category').pivotTable(
+      'project_categories'
+    )
+  }
 }
 
 module.exports = Project
